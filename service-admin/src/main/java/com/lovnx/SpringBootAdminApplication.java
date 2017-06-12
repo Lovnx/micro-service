1fa16c9760b105105360ae26401c1839c7d3d196
Administrator <Administrator@yzy>
2017-06-12 15:27:03 +0800

    spring boot admin整合

---
+++ service-admin/src/main/java/com/lovnx/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.lovnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
